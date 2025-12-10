85c87804d3f2953eb479d5180de80447cec2b065
bogale <aleksejbogoduhov2281337@gmail.com>
2025-12-10 13:14:21 +0900

    backup script preparation

--- modules/make_backup.sh
+++
@@ -1,6 +0,0 @@
-cd ~/backup
-for path in $(cat paths.bak); do
-    ln -s $path .
-done
-cd ~
-tar chzf backup.tar.gz backup
--- modules/setup.sh
+++ modules/setup.sh
@@ -1,3 +1,4 @@
+mkdir ~/local
 cd ~/modules
 cat domain bashrc >> ~/.bashrc
 rm domain bashrc ~/init.tar.gz
@@ -7,9 +8,7 @@ apt install -y speedtest-cli
 cd /etc/ssh
 sed -i "s/#\(PasswordAuthentication\).*/\1 no/" sshd_config
 rm -f sshd_config.d/*
-printf "Enter swap size: "
-read SWAP_SIZE
-fallocate -l ${SWAP_SIZE} /swapfile
+fallocate -l 1G /swapfile
 chmod 600 /swapfile
 mkswap /swapfile
 echo "/swapfile none swap sw 0 0" >> /etc/fstab
